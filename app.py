0bb0c53150cf0d3b5d84af437137d5f2b8bda0ac
Divya Jayendra Chaudhari <69595514+Divya-105@users.noreply.github.com>
2022-11-15 11:46:24 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import cv2
+# import streamlit as st
+
+st.title('Lane Detection')
+st.header('Upload a video and detect lane')
+
+st.header('Input images')
+
+f = st.file_uploader("Upload file")
+if f is not None:
+    file_details = {"FileName":f.name,"FileType":f.type}
+    st.write(file_details)
+    
